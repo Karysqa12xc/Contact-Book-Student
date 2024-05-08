1116b89aafd8be491793543b67ff9da9a2bcc1ee
Karysqa <85331340+Karysqa12xc@users.noreply.github.com>
2024-05-08 16:56:29 +0700

    fix conflict

--- src/app/controllers/SideController.js
+++ src/app/controllers/SideController.js
@@ -5,23 +5,40 @@ class SideController {
       if (!req.session.isLoggedIn) {
         return res.redirect("login");
       } else {
+<<<<<<< HEAD
         console.log(req.session.account);
         if(req.session.displayNotice){
           req.session.displayNotice = false;
         }
         res.render("home", {account: req.session.account, logged: req.session.isLoggedIn});
+=======
+        res.render("home", 
+        {
+          account: req.session.account,
+          isLoggedIn: req.session.isLoggedIn
+        });
+>>>>>>> fedeea3 (feat: tao lai kha nang dang nhap cho giao dien moi)
       }
     } catch (error) {
       res.status(500).json({message: `Internal server error + ${error}`});
     }
   }
   //[GET] /about
+<<<<<<< HEAD
   about(req, res){
     if (!req.session.isLoggedIn) {
       return res.redirect("login");
     }else{
       res.render("about", {account: req.session.account, logged: req.session.isLoggedIn});
     }
+=======
+  about(req, res, next){
+    res.render("about",  
+    {
+      account: req.session.account, 
+      isLoggedIn: req.session.isLoggedIn
+    });
+>>>>>>> fedeea3 (feat: tao lai kha nang dang nhap cho giao dien moi)
   }
 }
 
